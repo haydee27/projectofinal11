6ee15c524dd67ed7595b19b044057ea5a6d47261
Melani05 <melanibeltran68@gmail.com>
2022-11-09 12:18:37 -0300

    menunicaragua

---
+++ app/src/main/java/com/hb/recetaappfinal/MainActivitypostresn.java
@@ -0,0 +1,14 @@
+package com.hb.recetaappfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivitypostresn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activitypostresn);
+    }
+}
