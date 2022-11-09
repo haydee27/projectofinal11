3180d784c8368113820787002a194d87c2bfa0c8
haydee27 <otokeincognita@gmail.com>
2022-11-09 14:55:57 -0600

    Login y registro de usuarios

---
+++ app/src/main/java/com/hb/recetaappfinal/ui/login/registrar.java
@@ -0,0 +1,16 @@
+package com.hb.recetaappfinal.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hb.recetaappfinal.R;
+
+public class registrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
