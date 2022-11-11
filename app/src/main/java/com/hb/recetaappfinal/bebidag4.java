152e29c287224850ec59c85d77f46251a5c22781
Rocio104 <rociominero07@gmail.com>
2022-11-10 22:59:22 -0600

    guatemala

---
+++ app/src/main/java/com/hb/recetaappfinal/bebidag4.java
@@ -0,0 +1,14 @@
+package com.hb.recetaappfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bebidag4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bebidag4);
+    }
+}
