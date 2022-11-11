3be1c060ca0930fc7195032e05ff25d7457c6af8
Melani05 <melanibeltran68@gmail.com>
2022-11-11 13:24:58 -0300

    restauración

--- app/src/main/java/com/hb/recetaappfinal/ui/guatemala/MainActivitypostreg.java
+++ app/src/main/java/com/hb/recetaappfinal/ui/guatemala/MainActivitypostreg.java
@@ -16,43 +16,43 @@ import com.hb.recetaappfinal.ui.nicaragua.postren5;
 
 public class MainActivitypostreg extends AppCompatActivity {
 
-    Button btnio1, btnio2, btnio3, btnio4, btnio5;
+    Button btngo1, btngso2, btngo3, btngo4, btngo5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activitypostresn);
+        setContentView(R.layout.activity_main_activitypostreg);
 
-        btnio1 = findViewById(R.id.btnio1);
-        btnio2 = findViewById(R.id.btnio2);
-        btnio3 = findViewById(R.id.btnio3);
-        btnio4 = findViewById(R.id.btnio4);
-        btnio5 = findViewById(R.id.btnio5);
+        btngo1 = findViewById(R.id.btngo1);
+        btngso2 = findViewById(R.id.btngso2);
+        btngo3 = findViewById(R.id.btngo3);
+        btngo4 = findViewById(R.id.btngo4);
+        btngo5 = findViewById(R.id.btngo5);
     }
 
-    public void postren1(View view) {
-        Intent btnio1= new Intent(this, postren1.class);
-        startActivity(btnio1);
+    public void postreg1(View view) {
+        Intent btngo1= new Intent(this, postreg1.class);
+        startActivity(btngo1);
     }
 
-    public void postren2(View view) {
-        Intent btnio2= new Intent(this, postren2.class);
-        startActivity(btnio2);
+    public void postresg2(View view) {
+        Intent btngso2= new Intent(this, postresg2.class);
+        startActivity(btngso2);
     }
 
-    public void postren3(View view) {
-        Intent btnio3= new Intent(this, postren3.class);
-        startActivity(btnio3);
+    public void postreg3(View view) {
+        Intent btngo3= new Intent(this, postreg3.class);
+        startActivity(btngo3);
     }
 
-    public void postren4(View view) {
-        Intent btnio4= new Intent(this, postren4.class);
-        startActivity(btnio4);
+    public void postreg4(View view) {
+        Intent btngo4= new Intent(this, postreg4.class);
+        startActivity(btngo4);
     }
 
-    public void postren5(View view) {
-        Intent btnio5= new Intent(this, postren5.class);
-        startActivity(btnio5);
+    public void postreg5(View view) {
+        Intent btngo5= new Intent(this, postreg5.class);
+        startActivity(btngo5);
     }
 
 
