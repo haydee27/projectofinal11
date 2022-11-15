82033d613d60019e7653098a2e57fcc700e6dfae
haydee27 <otokeincognita@gmail.com>
2022-11-15 14:44:11 -0600

    button postres

--- app/src/main/java/com/hb/recetaappfinal/ui/categorias/subcategoriasp2.java
+++ app/src/main/java/com/hb/recetaappfinal/ui/categorias/subcategoriasp2.java
@@ -13,12 +13,14 @@ import com.hb.recetaappfinal.ui.costarica.postrec2;
 import com.hb.recetaappfinal.ui.costarica.postrec3;
 import com.hb.recetaappfinal.ui.costarica.postrec4;
 import com.hb.recetaappfinal.ui.costarica.postrec5;
+import com.hb.recetaappfinal.ui.honduras.postreh1;
+import com.hb.recetaappfinal.ui.honduras.postreh2;
 import com.hb.recetaappfinal.ui.nicaragua.postren4;
 import com.hb.recetaappfinal.ui.nicaragua.postren5;
 
 public class subcategoriasp2 extends AppCompatActivity {
 
-    Button btnio4, btnio5, btnco1, btnco2, btnco3, btnco4, btnco5;
+    Button btnio4, btnio5, btnco1, btnco2, btnco3, btnco4, btnco5, btnho1, btnho2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +33,8 @@ public class subcategoriasp2 extends AppCompatActivity {
         btnco3 = findViewById(R.id.btnco3);
         btnco4 = findViewById(R.id.btnco4);
         btnco5 = findViewById(R.id.btnco5);
+        btnho1 = findViewById(R.id.btnhho1);
+        btnho2 = findViewById(R.id.btnho2);
 
     }
 
@@ -69,4 +73,13 @@ public class subcategoriasp2 extends AppCompatActivity {
         startActivity(btnco5);
     }
 
+    public void postreh1(View view) {
+        Intent btnho1= new Intent(this, postreh1.class);
+        startActivity(btnho1);
+    }
+
+    public void postreh2(View view) {
+        Intent btnho2= new Intent(this, postreh2.class);
+        startActivity(btnho2);
+    }
 }
