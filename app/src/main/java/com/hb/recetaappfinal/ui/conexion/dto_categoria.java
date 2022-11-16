12ad3a3b19ca0634179fabab58aec645dae3497d
Melani05 <melanibeltran68@gmail.com>
2022-11-16 11:41:12 -0300

    categoría  recetas

--- app/src/main/java/com/hb/recetaappfinal/ui/conexion/dto_categoria.java
+++ app/src/main/java/com/hb/recetaappfinal/ui/conexion/dto_categoria.java
@@ -1,4 +1,35 @@
 package com.hb.recetaappfinal.ui.conexion;
 
 public class dto_categoria {
+
+    int id;
+    String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public dto_categoria() {
+
+
+    }
+
+    public dto_categoria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
 }
+
+
--- app/src/main/java/com/hb/recetaappfinal/ui/conexion/dto_recetas.java
+++ app/src/main/java/com/hb/recetaappfinal/ui/conexion/dto_recetas.java
@@ -1,4 +1,63 @@
 package com.hb.recetaappfinal.ui.conexion;
 
 public class dto_recetas {
+
+    int id_recetas;
+    String nombre;
+    String ingrediente;
+    String procedimiento;
+    int id;
+
+    public int getId_recetas() {
+        return id_recetas;
+    }
+
+    public void setId_recetas(int id_recetas) {
+        this.id_recetas = id_recetas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(String ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public String getProcedimiento() {
+        return procedimiento;
+    }
+
+    public void setProcedimiento(String procedimiento) {
+        this.procedimiento = procedimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public dto_recetas() {
+
+
+    }
+
+    public dto_recetas(int id_recetas, String nombre, String ingrediente, String procedimiento, int id) {
+        this.id_recetas = id_recetas;
+        this.nombre = nombre;
+        this.ingrediente = ingrediente;
+        this.procedimiento = procedimiento;
+        this.id = id;
+    }
 }
