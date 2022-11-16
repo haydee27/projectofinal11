55eb2222ba6a117db2739dcdaeff0a0c794477be
haydee27 <otokeincognita@gmail.com>
2022-11-15 23:24:31 -0600

    layout subcategoria

---
+++ app/src/main/java/com/hb/recetaappfinal/ui/categorias/subcategoria31.java
@@ -0,0 +1,16 @@
+package com.hb.recetaappfinal.ui.categorias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hb.recetaappfinal.R;
+
+public class subcategoria31 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subcategoria31);
+    }
+}
