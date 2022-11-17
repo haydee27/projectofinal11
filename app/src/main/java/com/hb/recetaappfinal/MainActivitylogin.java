c0f33d5132109dc80db2a84a3d86a2ca312615e5
Melani05 <melanibeltran68@gmail.com>
2022-11-17 00:22:51 -0300

    ...

--- app/src/main/java/com/hb/recetaappfinal/MainActivitylogin.java
+++ app/src/main/java/com/hb/recetaappfinal/MainActivitylogin.java
@@ -2,7 +2,6 @@ package com.hb.recetaappfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.hb.recetaappfinal.R;
 
 
 public class MainActivitylogin extends AppCompatActivity {
@@ -10,7 +9,7 @@ public class MainActivitylogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activitylogin);
+        setContentView(R.layout.activityl_login);
     }
 
 
